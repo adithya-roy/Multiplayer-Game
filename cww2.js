59c3df46d1ce7169658fe950c41f8bbcdba9c646
AN114 <55683353+AN114@users.noreply.github.com>
2019-10-20 20:39:39 +0400

    Update cww2.js
    working on wall collision

--- cww2.js
+++ cww2.js
@@ -1,60 +1,159 @@
-var snake;
-
-function startMovement() {
-    snake = new component(30, 25, "green", 10, 120);
-    playground.start();
+window.onload=function() {
+    canv=document.getElementById("MainCanvas");
+    ctx=canv.getContext("2d");
+    name = prompt("Please enter your name ");
+    document.addEventListener("keydown",keyPush);
+    document.getElementById("left").addEventListener("click",leftmove);
+    document.getElementById("right").addEventListener("click",rightmove);
+    document.getElementById("up").addEventListener("click",upmove);
+    document.getElementById("down").addEventListener("click",downmove);
+    document.getElementById("score").innerHTML = Score;
+    document.getElementById("playername").innerHTML = name;
+   Game= setInterval(game,1000/15);
 }
 
-var playground = {
-    canvas : document.getElementById("MainCanvas"),
-    start : function() {
-        this.canvas.width = 500;
-        this.canvas.height = 285;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updatePG, 20);
-    },
-    clear : function() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+Snake_Pos_x=Snake_Pos_y=10;
+grid_size=tiles_count=20;
+foodX=foodY=15;
+x_Speed=y_Speed=0;
+trail=[];
+tail = 1;
+Score= 0;
+var wall;
+name= "Unkown";
+function game() {
+   
+    Snake_Pos_x+=x_Speed;
+    Snake_Pos_y+=y_Speed;
+    if(Snake_Pos_x<0) {
+        Snake_Pos_x= tiles_count-1;
     }
-}
-
-function component(width, height, color, x, y) {
-    this.width = width;
-    this.height = height;
-    this.speedX = 0;
-    this.speedY = 0;
-    this.x = x;
-    this.y = y;    
-    this.update = function() {
-        cnt = playground.context;
-        cnt.fillStyle = color;
-        cnt.fillRect(this.x, this.y, this.width, this.height);
+    if(Snake_Pos_x>tiles_count-1) {
+        Snake_Pos_x= 0;
     }
-    this.newPos = function() {
-        this.x += this.speedX;
-        this.y += this.speedY;        
-    }    
+    if(Snake_Pos_y<0) {
+        Snake_Pos_y= tiles_count-1;
+    }
+    if(Snake_Pos_y>tiles_count-1) {
+        Snake_Pos_y= 0;
+    }
+    ctx.fillStyle="black";
+    ctx.fillRect(0,0,canv.width,canv.height);
+ 
+    ctx.fillStyle="lime";
+    for(var i=0;i<trail.length;i++) {
+        ctx.fillRect(trail[i].x*grid_size,trail[i].y*grid_size,grid_size,grid_size);
+        if(trail[i].x==Snake_Pos_x && trail[i].y==Snake_Pos_y) {
+            Reset();
+        }
+    }
+    trail.push({x:Snake_Pos_x,y:Snake_Pos_y});
+    while(trail.length>tail) {
+    trail.shift();
+    }
+ 
+    if(foodX==Snake_Pos_x && foodY==Snake_Pos_y) {
+        tail+=1;
+        Score += 1;
+        foodX=Math.floor(Math.random()*tiles_count);
+        foodY=Math.floor(Math.random()*tiles_count);
+        document.getElementById("score").innerHTML = Score;
+    }
+    var x1=120;
+    var x2=658;
+    var y1=950;
+    var y2=411;
+    if(Snake_Pos_y===x1 || Snake_Pos_y===x2|| Snake_Pos_x===y1 || Snake_Pos_x===y2){
+        Reset();
+    }
+
+    ctx.fillStyle="red";
+    ctx.fillRect(foodX*grid_size,foodY*grid_size,grid_size-2,grid_size-2);
+
+// ctx.fillStyle = "red";
+// ctx.fillRect(300,92,5,100);
+// ctx.fillRect(200,55,5,100);
+// ctx.fillRect(100,150,5,100);
+// ctx.fillRect(400,0,5,100);
+// ctx.fillRect(500,230,5,100);
+// ctx.fillRect(600,100,5,100);
+// ctx.fillRect(700,60,5,100);
+// ctx.fillRect(200,340,5,50);
+// ctx.fillRect(1,100,100,5);
+// ctx.fillRect(355,200,100,5);
+// ctx.fillRect(100,300,100,5);
+// ctx.fillRect(100,150,100,5);
+
+}
+function keyPush(evt) {
+    switch(evt.keyCode) {
+        case 37:
+            x_Speed=-1;y_Speed=0;
+            break;
+        case 38:
+            x_Speed=0;y_Speed=-1;
+            break;
+        case 39:
+            x_Speed=1;y_Speed=0;
+            break;
+        case 40:
+            x_Speed=0;y_Speed=1;
+            break;
+        case 65:
+            x_Speed=-1;y_Speed=0;
+            break;
+        case 87:
+            x_Speed=0;y_Speed=-1;
+            break;
+        case 68:
+            x_Speed=1;y_Speed=0;
+            break;
+        case 83:
+            x_Speed=0;y_Speed=1;
+            break;
+        
+            
+    }
+    
 }
 
-function updatePG() {
-    playground.clear();
-    snake.newPos();    
-    snake.update();
+function Reset(){
+
+    Snake_Pos_x=Snake_Pos_y=10;
+    grid_size=tiles_count=20;
+    foodX=foodY=15;
+    x_Speed=y_Speed=0;
+    trail=[];
+    tail = 1;
+    Score= 0;
+    clearInterval(Game);
+    Game = setInterval(game, 100);
+    document.getElementById("score").innerHTML = Score;
 }
 
-function up() {
-  snake.speedY -= 1; 
+fun
+
+function GameOver(){
+    
+
 }
 
-function down() {
-    snake.speedY += 1; 
-}
+function upmove() {
+    x_Speed=0;y_Speed=-1;
+  }
+  
+  function downmove() {
+    x_Speed=0;y_Speed=1;
+  }
+  
+  function leftmove() {
+    x_Speed=-1;y_Speed=0;
+  }
+  
+  function rightmove() {
+    x_Speed=1;y_Speed=0;
+  }
 
-function left() {
-    snake.speedX -= 1; 
-}
-
-function right() {
-    snake.speedX += 1; 
-}
+function End() {
+    alert("Game Over !!");
+  }
